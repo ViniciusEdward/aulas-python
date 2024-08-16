5b582e396f62cfd4b5733f5ec03eaed5e7eef259
ViniciusEdward <viniciusedward0307@gmail.com>
2024-08-16 18:14:13 +0000

    Comandos iniciais.

---
+++ 1.Tipos primitivos/tipagem dinamica.py
@@ -0,0 +1,12 @@
+import os
+
+os.system("cls || clear") # Limpa o terminal
+
+# Tipagem dinâmica
+nome = "Vinicius"
+idade = 17
+altura = 1.85
+
+print(f"Nome: {nome}")
+print(f"\nidade: {idade}")
+print(f"\nAltura: {altura}")
---
+++ 1.Tipos primitivos/tipagem estatica.py
@@ -0,0 +1,17 @@
+import os
+
+os.system("cls || clear") # Limpa o terminal
+
+# Tipagem estática
+nome : str # cadeia
+idade : int #inteiro
+altura : float #real
+
+nome = "Vinicius"
+idade = 17
+altura = 1.85
+
+print(f"Nome: {nome}")
+print(f"idade: {idade}")
+print(f"Altura: {altura}")
+

27abbc9460aa40213ec2dfcc12e98a2f7887ac51
ViniciusEdward <viniciusedward0307@gmail.com>
2024-08-30 21:47:19 +0000

    aula 30:08

--- 5. Laços de repetição while/5. Média aluno 3 notas e adicionar mais v2.py
+++ 5. Laços de repetição while/5. Média aluno 3 notas e adicionar mais v2.py
@@ -1,57 +1,53 @@
-import os
+"""
+Escreva um algoritmo que pergunte ao usuário se deseja inserir mais uma nota.
+Mostre um menu conforme o descrito abaixo:
+
+S – Inserir mais uma nota;
+P – Ver quantas notas foram inseridas;
+N – Calcular a média aritmética das notas informadas.
+
+O programa deve mostrar a média aritmética para o usuário.
+
+"""
+
+import os, time
 os.system("cls || clear")
 
 soma = 0
-soma_adicional = 0
+quantidade_notas = 0
 
-for i in range(3):
-    while True:
-        nota = float(input(f"Digite {i + 1}º nota: "))
-        soma += nota 
-        if nota < 0 or nota > 10:
-            os.system("cls || clear")
-            print("Nota inválida, digite novamente.")
-        else:
-            break
-print("""
-1 - S (Adicionar mais uma nota)
-2 - P (Mostrar notas inseridas)
-3 - N (Calcular média)
-     """)
-    
-adicionar = input("\nDeseja adicionar mais uma nota? (S | P | N): ").upper()
 
-match(adicionar):
-    case "S":
-        qntd_notas = int(input("Informe a quantidade de notas: "))
-
-        for i in range(qntd_notas):
-            while True:
-                nota_adicional = float(input(f"\nDigite {i + 4}º nota: "))
-                soma_adicional += nota_adicional
-                if nota_adicional < 0 or nota_adicional > 10:
-                    os.system("cls || clear")
-                    print("Nota inválida, digite novamente.")
-                else:
-                    break
-        media = (soma + soma_adicional) / (qntd_notas + 3)
-        
-
-    case "P": 
-        for i in range(3):
-            print(f"Nota: {nota:.2f}")
-            media = soma / 3
+while True:
+     print("""
+           1 - S (Adicionar mais uma nota)
+           2 - P (Mostrar notas inseridas)
+           3 - N (Calcular média)
+           """)
+     resposta = input("\nDeseja adicionar mais uma nota? (S | P | N): ").upper()
+     match(resposta):
+        case "S":
+            nota = float(input(f"Digite uma nota: "))
             soma += nota
+            quantidade_notas +=1
 
-    case "N":
-        media = soma / 3
-        soma += nota
+        case "P": 
+            if quantidade_notas == 0:
+                print("Não foi inseridas notas.\n")
+                input("Pressione uma tecla para continuar...")
+                time.sleep(3)
+            else:
+                print(f"Quantidade de notas inseridas: {quantidade_notas} \n")
+                input("Pressione uma tecla para continuar...")
 
+        case "N":
+            if quantidade_notas == 0:
+                print("Não foram inseridas notas. \n")
+            else:
+                break
+
+        case _:
+            print("Opção inválida... \n")
+            time.sleep(3)
+
+media = soma / quantidade_notas
 print(f"\nMédia: {media:.2f}")
-
-if media >= 7:
-    print("Parabéns, você está aprovado!")
-elif media >= 5 and media < 7:
-    print("Infelizmente você não conseguiu a atingir a nota esperada. Recuperação")
-elif media < 5:
-    print("Sinto muito. Reprovado")

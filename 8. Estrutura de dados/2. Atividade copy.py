a96bf808531a93a70501537afdf7856ac467db64
ViniciusEdward <viniciusedward0307@gmail.com>
2024-10-03 16:53:54 +0000

    salvando

--- 8. Estrutura de dados/2. Atividade copy.py
+++ 8. Estrutura de dados/2. Atividade copy.py
@@ -6,6 +6,18 @@ lista_familia = []
 lista_salario = []
 contador = 0
 
+def mediafilhos(n1, n2):
+    return n1 / n2 
+
+def mediasalario(n1, n2):
+    return n1 / n2 
+
+def maiorsalario(n1):
+    return max(n1) 
+
+def menorsalario(n1):
+    return min(n1)
+
 while True :
     print("""
         \t=== MENU ===
@@ -26,17 +38,17 @@ while True :
             lista_salario.append(salario)
 
         case "E":
-            maior_salario = max(lista_salario)
-            menor_salario = min(lista_salario)
+            maior_salario = maiorsalario(lista_salario)
+            menor_salario = menorsalario(lista_salario)
             soma_salario = sum(lista_salario)
             soma_filhos = sum(lista_familia)
-            media_filhos = soma_filhos / contador
-            media_salario = soma_salario / contador
+            media_filhos = mediafilhos(soma_filhos, contador)
+            media_salario = mediasalario(soma_salario, contador)
             print(f"Total de famílias que responderam a pesquisa: {contador}")
-            print(f"Média do salário da população: {media_salario}")
-            print(f"Média do número de filhos: {media_filhos}")
-            print(f"Maior salário: {maior_salario}")
-            print(f"Menor salário: {menor_salario}")
+            print(f"Média do salário da população: R${media_salario:.2f}")
+            print(f"Média do número de filhos: {media_filhos:.2f}")
+            print(f"Maior salário: R${maior_salario:.2f}")
+            print(f"Menor salário: R${menor_salario:.2f}")
             break
 
         case "S":
@@ -52,9 +64,14 @@ nome_do_arquivo = "pesquisa_prefeitura.txt"
 # Abrindo arquivo e definindo que será feita a escrita de dados.
 with open(nome_do_arquivo, "w") as arquivo_familia:
     # Percorrendo vetor/lista.
-    for familia in lista_familia, lista_salario:
+    for i in range(1):
         #Escrevendo no arquivo uma linha de cada vez.
-        arquivo_familia.write(f"Total de famílias que responderam: {contador}, Média salarial: {media_salario}, Média do número de filhos: {media_filhos}, Maior salário: {maior_salario}, Menor salário: {menor_salario}\n")
+        arquivo_familia.write(f"""
+Total de famílias que responderam: {contador} 
+Média salarial: {media_salario:.2f} 
+Média do número de filhos: {media_filhos:.2f}
+Maior salário: R${maior_salario:.2f} 
+Menor salário: R${menor_salario:.2f}""")
 
 arquivo_familia.close()
 
